838c0a393b20303b7ccae523e679c61c0e7e8053
okta467 <oktaalfiansyah@gmail.com>
2024-07-01 23:11:22 +0700

    Remove delete data in tbl_pengguna in alumni_hapus page for role admin
    This is due to creation of database trigger in the last commit
    which is deletes data in tbl_pengguna after deletion of
    tbl_alumni based on id_pengguna

--- admin/alumni_hapus.php
+++ admin/alumni_hapus.php
@@ -11,66 +11,19 @@
     include_once '../config/connection.php';
     
     $id_alumni = $_GET['xid_alumni'];
-    $id_pengguna = $_GET['xid_pengguna'];
 
-    mysqli_autocommit($connection, false);
+    $stmt = mysqli_stmt_init($connection);
 
-    $success = true;
+    mysqli_stmt_prepare($stmt, "DELETE FROM tbl_alumni WHERE id=?");
+    mysqli_stmt_bind_param($stmt, 'i', $id_alumni);
 
-    try {
-        $stmt_alumni = mysqli_stmt_init($connection);
-        $query_alumni = "DELETE FROM tbl_alumni WHERE id=?";
+    $delete = mysqli_stmt_execute($stmt);
 
-        if (!mysqli_stmt_prepare($stmt_alumni, $query_alumni)) {
-            $_SESSION['msg'] = 'Transaction Pegawai failed: ' . mysqli_stmt_error($stmt_alumni);
-            echo "<meta http-equiv='refresh' content='0;alumni.php?go=alumni'>";
-            return;
-        }
-        
-        mysqli_stmt_bind_param($stmt_alumni, 'i', $id_alumni);
+    !$delete
+        ? $_SESSION['msg'] = 'other_error'
+        : $_SESSION['msg'] = 'delete_success';
 
-        if (!mysqli_stmt_execute($stmt_alumni)) {
-            $_SESSION['msg'] = 'Transaction Pegawai failed: ' . mysqli_stmt_error($stmt_alumni);
-            echo "<meta http-equiv='refresh' content='0;alumni.php?go=alumni'>";
-            return;
-        }
-
-        $stmt_pengguna = mysqli_stmt_init($connection);
-        $query_pengguna = "DELETE FROM tbl_pengguna WHERE id=?";
-
-        if (!mysqli_stmt_prepare($stmt_pengguna, $query_pengguna)) {
-            $_SESSION['msg'] = 'Transaction Pengguna failed: ' . mysqli_stmt_error($stmt_pengguna);
-            echo "<meta http-equiv='refresh' content='0;alumni.php?go=alumni'>";
-            return;
-        }
-        
-        mysqli_stmt_bind_param($stmt_pengguna, 'i', $id_pengguna);
-
-        if (!mysqli_stmt_execute($stmt_pengguna)) {
-            $_SESSION['msg'] = 'Transaction Pengguna failed: ' . mysqli_stmt_error($stmt_pengguna);
-            echo "<meta http-equiv='refresh' content='0;alumni.php?go=alumni'>";
-            return;
-        }
-
-        if (!mysqli_commit($connection)) {
-            $_SESSION['msg'] = 'Transaction commit failed: ' . mysqli_stmt_error($stmt_alumni);
-            echo "<meta http-equiv='refresh' content='0;alumni.php?go=alumni'>";
-            return;
-        }
-    } catch (Exception $e) {
-        // Roll back the transaction if any statement fails
-        $success = false;
-        mysqli_rollback($connection);
-        $_SESSION['msg'] = 'Transaction failed: ' . $e->getMessage();
-    }
-
-    !$success
-        ? ''
-        : $_SESSION['msg'] = 'save_success';
-
-    mysqli_stmt_close($stmt_alumni);
-    mysqli_stmt_close($stmt_pengguna);
-
+    mysqli_stmt_close($stmt);
     mysqli_close($connection);
 
     echo "<meta http-equiv='refresh' content='0;alumni.php?go=alumni'>";
