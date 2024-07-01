432b32f7daf3781a0587e9d52b19ed87339c1328
okta467 <oktaalfiansyah@gmail.com>
2024-07-01 23:49:47 +0700

    Remove delete data in tbl_pengguna in perusahaan_hapus page for role admin
    This is due to creation of database trigger in the last commit
    which is deletes data in tbl_pengguna after deletion of
    tbl_perusahaan based on id_pengguna

--- admin/perusahaan_hapus.php
+++ admin/perusahaan_hapus.php
@@ -11,66 +11,19 @@
     include_once '../config/connection.php';
     
     $id_perusahaan = $_GET['xid_perusahaan'];
-    $id_pengguna = $_GET['xid_pengguna'];
 
-    mysqli_autocommit($connection, false);
+    $stmt = mysqli_stmt_init($connection);
 
-    $success = true;
+    mysqli_stmt_prepare($stmt, "DELETE FROM tbl_perusahaan WHERE id=?");
+    mysqli_stmt_bind_param($stmt, 'i', $id_perusahaan);
 
-    try {
-        $stmt_perusahaan = mysqli_stmt_init($connection);
-        $query_perusahaan = "DELETE FROM tbl_perusahaan WHERE id=?";
+    $delete = mysqli_stmt_execute($stmt);
 
-        if (!mysqli_stmt_prepare($stmt_perusahaan, $query_perusahaan)) {
-            $_SESSION['msg'] = 'Transaction Pegawai failed: ' . mysqli_stmt_error($stmt_perusahaan);
-            echo "<meta http-equiv='refresh' content='0;perusahaan.php?go=perusahaan'>";
-            return;
-        }
-        
-        mysqli_stmt_bind_param($stmt_perusahaan, 'i', $id_perusahaan);
+    !$delete
+        ? $_SESSION['msg'] = 'other_error'
+        : $_SESSION['msg'] = 'delete_success';
 
-        if (!mysqli_stmt_execute($stmt_perusahaan)) {
-            $_SESSION['msg'] = 'Transaction Pegawai failed: ' . mysqli_stmt_error($stmt_perusahaan);
-            echo "<meta http-equiv='refresh' content='0;perusahaan.php?go=perusahaan'>";
-            return;
-        }
-
-        $stmt_pengguna = mysqli_stmt_init($connection);
-        $query_pengguna = "DELETE FROM tbl_pengguna WHERE id=?";
-
-        if (!mysqli_stmt_prepare($stmt_pengguna, $query_pengguna)) {
-            $_SESSION['msg'] = 'Transaction Pengguna failed: ' . mysqli_stmt_error($stmt_pengguna);
-            echo "<meta http-equiv='refresh' content='0;perusahaan.php?go=perusahaan'>";
-            return;
-        }
-        
-        mysqli_stmt_bind_param($stmt_pengguna, 'i', $id_pengguna);
-
-        if (!mysqli_stmt_execute($stmt_pengguna)) {
-            $_SESSION['msg'] = 'Transaction Pengguna failed: ' . mysqli_stmt_error($stmt_pengguna);
-            echo "<meta http-equiv='refresh' content='0;perusahaan.php?go=perusahaan'>";
-            return;
-        }
-
-        if (!mysqli_commit($connection)) {
-            $_SESSION['msg'] = 'Transaction commit failed: ' . mysqli_stmt_error($stmt_perusahaan);
-            echo "<meta http-equiv='refresh' content='0;perusahaan.php?go=perusahaan'>";
-            return;
-        }
-    } catch (Exception $e) {
-        // Roll back the transaction if any statement fails
-        $success = false;
-        mysqli_rollback($connection);
-        $_SESSION['msg'] = 'Transaction failed: ' . $e->getMessage();
-    }
-
-    !$success
-        ? ''
-        : $_SESSION['msg'] = 'save_success';
-
-    mysqli_stmt_close($stmt_perusahaan);
-    mysqli_stmt_close($stmt_pengguna);
-
+    mysqli_stmt_close($stmt);
     mysqli_close($connection);
 
     echo "<meta http-equiv='refresh' content='0;perusahaan.php?go=perusahaan'>";
